b5035851840d6c75da7d985c7c81e2293b984847
Gustavo <moreira.andrade@aluno.ifsp.edu.br>
2026-06-02 12:30:55 -0300

    Estrutura das Páginas + Arquivos (AG)

---
+++ scripts/scripts.js
@@ -0,0 +1,5 @@
+/* Links que podem ajudar com o LocalStorage:
+    https://blog.formacao.dev/o-que-e-local-storage/
+    https://www.youtube.com/watch?v=ewnyMjQjda0
+    https://www.youtube.com/watch?v=xNLxJ8IraFA
+*/
