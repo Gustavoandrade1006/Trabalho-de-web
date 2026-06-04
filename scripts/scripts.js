0a3184388d80951ab0acdab0b61961ddd883dc4f
Gustavo <gustavo.deandrademoreira@gmail.com>
2026-06-04 15:46:13 -0300

    Muita coisa, imagens, funcao do sidebar, etc

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,3 +3,19 @@
     https://www.youtube.com/watch?v=ewnyMjQjda0
     https://www.youtube.com/watch?v=xNLxJ8IraFA
 */
+const menuToggle = document.getElementById('menu-toggle');
+const overlay = document.getElementById('overlay');
+
+menuToggle.addEventListener('change', () => {
+    if (menuToggle.checked) {
+        overlay.classList.add('ativo');
+    } else {
+        overlay.classList.remove('ativo');
+    }
+});
+
+// aqui quando clica no overlay fecha o sidebar também
+overlay.addEventListener('click', () => {
+    menuToggle.checked = false;
+    overlay.classList.remove('ativo');
+});
