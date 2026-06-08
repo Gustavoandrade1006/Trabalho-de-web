237ddc807a48fb3a87d12c56ae96453081913f8a
itoikk <itoiandre14@gmail.com>
2026-06-08 17:29:53 -0300

    adicionando o nome, email e senha ao localStorage alem do botao sair apagar tudo

--- scripts/login.js
+++ scripts/login.js
@@ -1,3 +1,15 @@
-function login(nome, email, senha) {
-    return;
+function login() {
+    const nome = document.getElementById("nomeInput").value;
+    const email = document.getElementById("emailInput").value;
+    // Senhas no local storage não é seguro!
+    const senha = document.getElementById("senhaInput").value;
+
+    if (nome == "" || email == "" || senha == "") {
+        return;
+    } else {
+        localStorage.setItem("nome-usuario", nome);
+        localStorage.setItem("email-usuario", email);
+        localStorage.setItem("senha-usuario", senha);
+    }
 }
+
